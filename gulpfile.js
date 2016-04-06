a2c6d432c5d88fb94cfd569a83a58607bd5898f5
bonecrushereb <bonecrushereb@gmail.com>
2016-04-06 09:21:27 -0700

    added gulpfile.js and package.json to run gulp functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,37 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
+var files = ['index.js', 'lib/**/*.js', 'bin/*', 'gulpfile.js'];
+
 gulp.task('lint:test', () => {
   return gulp.src('./test/**/*test.js')
     .pipe(eslint({
+      rules: {
+        'indent': ['error', 2]
+      },
       envs : [
-        'mocha'
+        'mocha',
+        'es6'
       ]
     }))
     .pipe(eslint.format());
 });
+
+gulp.task('lint:nontest', () => {
+  return gulp.src(files)
+    .pipe(eslint({
+      rules: {
+        'indent': ['error', 2]
+      },
+      envs : [
+        'es6'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task ('watch', () => {
+  gulp.watch(['**/*'], ['lint']);
+});
+
+gulp.task('default', [ 'lint:test', 'lint:nontest']);
